c05835bfa20cad9c6b3ddf82dd55f9d869b5e1b2
anvargasl <anvargas@udel.edu>
2024-02-12 15:52:23 -0500

    deleting experimental results folder

--- python_scripts/sum_energies.py
+++ python_scripts/sum_energies.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 #read data
 runs = int(input("enter number of runs: "))
@@ -15,10 +16,29 @@ Events = len(data_frames[0].index)
 
 print("Tot events =", Events)
 
-data_frames[0] = data_frames[0].eval('Sum = Cerenkov + CoupledTransportation + eIon + msc + phot + compt') 
+data_frames[0] = data_frames[0].eval('Sum = Cerenkov+compt+CoupledTransportation+eBrem+eIon+msc+phot') 
 
-data_frames[0] = data_frames[0].eval('Diff = Tot - Sum') 
+def err(row):
+    return np.abs(row.Tot-row.Sum)/row.Tot
+
+diff_list  = [(t-s)/t if t>0 else 0 for t, s in zip(data_frames[0]["Tot"], data_frames[0]["Sum"])]
+
+data_frames[0]["Diff"] = diff_list
+#data_frames[0]["Diff"] = data_frames[0].apply(err, axis=1) 
+#data_frames[0] = data_frames[0].eval('Diff = (Tot - Sum)/Tot') 
 
 for e in range(Events):
-    if data_frames[0]["Diff"][e] != 0:
-        print(e, data_frames[0]["Diff"][e])
+    if np.abs(data_frames[0]["Diff"][e]) >= 0.01:
+        print(e, data_frames[0]["Tot"][e], data_frames[0]["Sum"][e], data_frames[0]["Diff"][e])
+
+weird_events = data_frames[0].index[data_frames[0]['Tot'] >= 662].tolist()
+
+tot_energy = [t for t in data_frames[0]['Tot'] if t>0]
+print(max(tot_energy))
+
+bins = np.arange(0, 671, 10)
+counts, _ = np.histogram(tot_energy, bins=bins)
+
+plt.step(bins[:-1], counts, where="mid")
+plt.axvline(x=477, label="borde Compton", color="orange")
+plt.savefig("../figures/100000events_run0_nt_Event_t0.pdf", bbox_inches="tight")
